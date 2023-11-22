90e2cc9ac639dc18b56c743be4f8d806628c284b
Thegreatecookie <hatrikhang@yahoo.com.vn>
2023-11-23 02:12:02 +0700

    2st

--- fe-web-ban-hang/src/App.js
+++ fe-web-ban-hang/src/App.js
@@ -2,10 +2,12 @@ import logo from "./logo.svg";
 import "./App.css";
 import JoySignInSideTemplate from "./scenes/signin/signin";
 import ButtonAppBar from "./scenes/signin/global/topbar";
+import MiniDrawer from "./scenes/signin/global/sidebar";
 function App() {
   return (
     <div className="App">
-      <ButtonAppBar />
+      {/* <ButtonAppBar /> */}
+      <MiniDrawer />
     </div>
   );
 }
