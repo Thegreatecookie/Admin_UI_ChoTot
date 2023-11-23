e4e549feb340961472292aac98d0830271b037bf
Thegreatecookie <hatrikhang@yahoo.com.vn>
2023-11-23 17:28:17 +0700

    3st

--- fe-web-ban-hang/src/App.js
+++ fe-web-ban-hang/src/App.js
@@ -1,13 +1,12 @@
 import logo from "./logo.svg";
 import "./App.css";
 import JoySignInSideTemplate from "./scenes/signin/signin";
-import ButtonAppBar from "./scenes/signin/global/topbar";
-import MiniDrawer from "./scenes/signin/global/sidebar";
+import JoyOrderDashboardTemplate from "./scenes/dashboard/components/Sidebar-exe";
 function App() {
   return (
     <div className="App">
       {/* <ButtonAppBar /> */}
-      <MiniDrawer />
+      <JoyOrderDashboardTemplate />
     </div>
   );
 }
